74ec8022255fec8469c98903bd9dff05d4bd46c9
WiliamMelo <wiliammelo.mota@gmail.com>
2024-06-26 20:37:21 -0300

    Project initialized

---
+++ src/main/java/org/wiliammelo/empoweru/EmpowerUApplication.java
@@ -0,0 +1,13 @@
+package org.wiliammelo.empoweru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpowerUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpowerUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wiliammelo/empoweru/EmpowerUApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wiliammelo.empoweru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpowerUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
