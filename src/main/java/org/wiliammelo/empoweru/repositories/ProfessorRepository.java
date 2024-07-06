3b8fdc29800f88ec2ab29a59f1fe56ac47f007b6
WiliamMelo <wiliammelo.mota@gmail.com>
2024-07-06 14:03:13 -0300

    Removed unused queries

--- src/main/java/org/wiliammelo/empoweru/repositories/ProfessorRepository.java
+++ src/main/java/org/wiliammelo/empoweru/repositories/ProfessorRepository.java
@@ -6,15 +6,10 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.wiliammelo.empoweru.models.Professor;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProfessorRepository extends CrudRepository<Professor, UUID> {
-
-    @Query("SELECT p FROM Professor p WHERE p.user.email = :email")
-    Optional<Professor> findByEmail(@Param("email") String email);
-
     @Query("SELECT p FROM Professor p WHERE p.user.id = :userId")
     Professor findByUserId(@Param("userId") UUID id);
 
--- src/main/java/org/wiliammelo/empoweru/repositories/StudentRepository.java
+++ src/main/java/org/wiliammelo/empoweru/repositories/StudentRepository.java
@@ -6,14 +6,10 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.wiliammelo.empoweru.models.Student;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface StudentRepository extends CrudRepository<Student, UUID> {
-
-    @Query("SELECT s FROM Student s WHERE s.user.email = :email")
-    Optional<Student> findByEmail(@Param("email") String email);
 
     @Query("SELECT s FROM Student s WHERE s.user.id = :userId")
     Student findByUserId(@Param("userId") UUID userId);
